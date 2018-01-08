fda2fd9e39ea9115330cd5ccd98fbac377d9ef78
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-08 16:44:43 +0100

    Update boobs.js

--- commands/boobs.js
+++ commands/boobs.js
@@ -7,8 +7,6 @@ exports.run = (client, message, args) => {
     var min = 10000;
     var MathRan = Math.floor(Math.random() * (max - min + 0)) + min;
     var MathLoL = Math.round(MathRan);
-    if (!message.channel.nsfw) {
-        message.channel.send(":underage: NSFW Command. Please switch to NSFW channel in order to use this command.")
     } else {   
         var randomname = Math.floor(Math.random() * (99999999999999999999 - 11111111111111111111 + 0)) + 11111111111111111111;
         request.get("http://media.oboobs.ru/boobs_preview/" + MathLoL + ".jpg").then(r => {
