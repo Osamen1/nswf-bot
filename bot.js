86d557f2034cf859829fe75b46f48b8deb1399d7
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-08 15:56:06 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    client.user.setPresence({game: {name: "nsfw help", type: 0}});
+    console.log("I am ready!");
+});
+
+client.on("message", message => {
+    if (message.author.bot) return;
+    if (message.content.indexOf(prefix) !== 0) return;
+    if (message.channel.type === "dm") return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    try {
+        let commandFile = require(`./commands/${command}.js`);
+        commandFile.run(client, message, args);
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
