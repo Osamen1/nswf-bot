24d9eb7852b17711da459d8d390b8c9b01760d54
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-08 16:23:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,108 +1,14 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on("ready", () => {
-    client.user.setPresence({game: {name: "nsfw help", type: 0}});
-    console.log("I am ready!");
-
-    request
-        .post(`https://bots.discord.pw/api/bots/${client.user.id}/stats`)
-        .send(`{ "server_count": ${client.guilds.size},
-    "shards": [${client.guilds.size}],
-    "shard_count": ${client.guilds.size} }`)
-        .type('application/json')
-        .set('Authorization', discordpwtoken)
-        .set('Accept', 'application/json')
-        .end(err => {
-            if (err) return console.error(err);
-            console.log("Posted stats to bots.discord.pw!");
-        });
-    request
-        .post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
-        .send(`{ "server_count": ${client.guilds.size},
-    "shards": [${client.guilds.size}],
-    "shard_count": ${client.guilds.size} }`)
-        .type('application/json')
-        .set('Authorization', discordbotsorgtoken)
-        .set('Accept', 'application/json')
-        .end(err => {
-            if (err) return console.error(err);
-            console.log("Posted stats to discordbots.org!");
-        });
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-client.on("guildCreate", guild => {
-    client.channels.get("397193015674011659").send(`New guild joined!\nName: ${guild.name}\nMember Count: ${guild.memberCount}.\nNow im in **${client.guilds.size}** guilds.`)
-
-    request
-        .post(`https://bots.discord.pw/api/bots/${client.user.id}/stats`)
-        .send(`{ "server_count": ${client.guilds.size},
-    "shards": [${client.guilds.size}],
-    "shard_count": ${client.guilds.size} }`)
-        .type('application/json')
-        .set('Authorization', discordpwtoken)
-        .set('Accept', 'application/json')
-        .end(err => {
-            if (err) return console.error(err);
-            console.log("Posted stats to bots.discord.pw!");
-        });
-    request
-        .post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
-        .send(`{ "server_count": ${client.guilds.size},
-    "shards": [${client.guilds.size}],
-    "shard_count": ${client.guilds.size} }`)
-        .type('application/json')
-        .set('Authorization', discordbotsorgtoken)
-        .set('Accept', 'application/json')
-        .end(err => {
-            if (err) return console.error(err);
-            console.log("Posted stats to discordbots.org!");
-        });
-});
-
-client.on("guildDelete", guild => {
-    client.channels.get("397193015674011659").send(`Guild left...\nName: ${guild.name}\nMember Count: ${guild.memberCount}.\n\nNow im in **${client.guilds.size}** guilds.`)
-
-    request
-    .post(`https://bots.discord.pw/api/bots/${client.user.id}/stats`)
-    .send(`{ "server_count": ${client.guilds.size},
-"shards": [${client.guilds.size}],
-"shard_count": ${client.guilds.size} }`)
-    .type('application/json')
-    .set('Authorization', discordpwtoken)
-    .set('Accept', 'application/json')
-    .end(err => {
-        if (err) return console.error(err);
-        console.log("Posted stats to bots.discord.pw!");
-    });
-request
-    .post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
-    .send(`{ "server_count": ${client.guilds.size},
-"shards": [${client.guilds.size}],
-"shard_count": ${client.guilds.size} }`)
-    .type('application/json')
-    .set('Authorization', discordbotsorgtoken)
-    .set('Accept', 'application/json')
-    .end(err => {
-        if (err) return console.error(err);
-        console.log("Posted stats to discordbots.org!");
-    });
-});
-
-client.on("message", message => {
-    if (message.author.bot) return;
-    if (message.content.indexOf(prefix) !== 0) return;
-    if (message.channel.type === "dm") return;
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-
-    try {
-        let commandFile = require(`./commands/${command}.js`);
-        commandFile.run(client, message, args);
-    } catch (err) {
-        console.error(err);
-    }
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
