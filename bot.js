5d9523d93a4b23f637f21216cd0aeb933f18272b
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-08 17:02:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 //----------------------------------------------
-var token = "Mzk5OTI3Nzg3NTc1NzA1NjEx.DTUNVw.ZkzQDN4EOslVPJexWD8p03Cx7Lo"
+var token = ""
 var prefix = "nsfw "
 var discordbotsorgtoken = ""
 var discordpwtoken = ""
@@ -11,6 +11,89 @@ var discordpwtoken = ""
 client.on("ready", () => {
     client.user.setPresence({game: {name: "nsfw help", type: 0}});
     console.log("I am ready!");
+
+    request
+        .post(`https://bots.discord.pw/api/bots/${client.user.id}/stats`)
+        .send(`{ "server_count": ${client.guilds.size},
+    "shards": [${client.guilds.size}],
+    "shard_count": ${client.guilds.size} }`)
+        .type('application/json')
+        .set('Authorization', discordpwtoken)
+        .set('Accept', 'application/json')
+        .end(err => {
+            if (err) return console.error(err);
+            console.log("Posted stats to bots.discord.pw!");
+        });
+    request
+        .post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
+        .send(`{ "server_count": ${client.guilds.size},
+    "shards": [${client.guilds.size}],
+    "shard_count": ${client.guilds.size} }`)
+        .type('application/json')
+        .set('Authorization', discordbotsorgtoken)
+        .set('Accept', 'application/json')
+        .end(err => {
+            if (err) return console.error(err);
+            console.log("Posted stats to discordbots.org!");
+        });
+});
+
+client.on("guildCreate", guild => {
+    client.channels.get("397193015674011659").send(`New guild joined!\nName: ${guild.name}\nMember Count: ${guild.memberCount}.\nNow im in **${client.guilds.size}** guilds.`)
+
+    request
+        .post(`https://bots.discord.pw/api/bots/${client.user.id}/stats`)
+        .send(`{ "server_count": ${client.guilds.size},
+    "shards": [${client.guilds.size}],
+    "shard_count": ${client.guilds.size} }`)
+        .type('application/json')
+        .set('Authorization', discordpwtoken)
+        .set('Accept', 'application/json')
+        .end(err => {
+            if (err) return console.error(err);
+            console.log("Posted stats to bots.discord.pw!");
+        });
+    request
+        .post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
+        .send(`{ "server_count": ${client.guilds.size},
+    "shards": [${client.guilds.size}],
+    "shard_count": ${client.guilds.size} }`)
+        .type('application/json')
+        .set('Authorization', discordbotsorgtoken)
+        .set('Accept', 'application/json')
+        .end(err => {
+            if (err) return console.error(err);
+            console.log("Posted stats to discordbots.org!");
+        });
+});
+
+client.on("guildDelete", guild => {
+    client.channels.get("397193015674011659").send(`Guild left...\nName: ${guild.name}\nMember Count: ${guild.memberCount}.\n\nNow im in **${client.guilds.size}** guilds.`)
+
+    request
+    .post(`https://bots.discord.pw/api/bots/${client.user.id}/stats`)
+    .send(`{ "server_count": ${client.guilds.size},
+"shards": [${client.guilds.size}],
+"shard_count": ${client.guilds.size} }`)
+    .type('application/json')
+    .set('Authorization', discordpwtoken)
+    .set('Accept', 'application/json')
+    .end(err => {
+        if (err) return console.error(err);
+        console.log("Posted stats to bots.discord.pw!");
+    });
+request
+    .post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
+    .send(`{ "server_count": ${client.guilds.size},
+"shards": [${client.guilds.size}],
+"shard_count": ${client.guilds.size} }`)
+    .type('application/json')
+    .set('Authorization', discordbotsorgtoken)
+    .set('Accept', 'application/json')
+    .end(err => {
+        if (err) return console.error(err);
+        console.log("Posted stats to discordbots.org!");
+    });
 });
 
 client.on("message", message => {
@@ -28,6 +111,7 @@ client.on("message", message => {
         console.error(err);
     }
 });
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
