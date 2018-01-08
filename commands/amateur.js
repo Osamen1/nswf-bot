9b7a6e23f2f61f00923cd419a7a0ba1fd1fa9787
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-08 16:43:10 +0100

    Update amateur.js

--- commands/amateur.js
+++ commands/amateur.js
@@ -1,7 +1,3 @@
-const randomPuppy = require('random-puppy');
-const request = require('snekfetch');
-const fs = require("fs")
-
 exports.run = (client, message, args) => {
     if (!message.channel.nsfw) return message.channel.send(":underage: NSFW Command. Please switch to NSFW channel in order to use this command.")
 
