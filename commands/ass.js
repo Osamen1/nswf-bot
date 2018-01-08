1c5ed7d584d7c2525018cedb1097ec4e9bddef51
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-08 18:45:24 +0100

    Add files via upload

--- commands/ass.js
+++ commands/ass.js
@@ -6,9 +6,8 @@ exports.run = (client, message, args) => {
     var max = 5244;
     var min = 1000;
     var MathRan = Math.floor(Math.random() * (max - min + 0)) + min;
-    var MathLoL = Math.round(MathRan);
-    } else {   
-        var randomname = Math.floor(Math.random() * (99999999999999999999 - 11111111111111111111 + 0)) + 11111111111111111111;
+    var MathLoL = Math.round(MathRan); 
+    var randomname = Math.floor(Math.random() * (99999999999999999999 - 11111111111111111111 + 0)) + 11111111111111111111;
         request.get("http://media.obutts.ru/butts_preview/0" + MathLoL + ".jpg").then(r => {
             fs.writeFile(`${randomname}.jpg`, r.body)
             message.channel.sendFile(r.body).then(d => {
