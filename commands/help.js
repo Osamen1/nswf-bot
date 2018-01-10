9b21affca033c795bc35df2986464b78c6ba8f24
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-10 21:03:37 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send("```NSFW Komendy:\n 4k\n amateur\n ass\n asian\n boobs\n cosplay\n gif\n hentai\n fuck\n nude\n pornhub\n pussy\n\nInne Komendy: \n help```")
+    message.channel.send("```NSFW Komendy:\n 4k\n amateur\n ass\n asian\n boobs\n cosplay\n gif\n hentai\n fuck\n pornhub\n pussy\n\nInne Komendy: \n help```")
 }
