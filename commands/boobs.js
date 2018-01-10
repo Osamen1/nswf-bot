183f998c1656d98f81ef51a2e0d0ac57bb052541
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-10 19:46:36 +0100

    Update boobs.js

--- commands/boobs.js
+++ commands/boobs.js
@@ -13,7 +13,7 @@ exports.run = (client, message, args) => {
     randomPuppy(sub)
             .then(url => {
                 var randomname = Math.floor(Math.random() * (99999999999999999999 - 11111111111111111111 + 0)) + 11111111111111111111;
-                request.get("url").then(r => {               
+                request.get(url).then(r => {               
                     fs.writeFile(`${randomname}.jpg`)
                     message.channel.send(url).then(d => {
                     })
