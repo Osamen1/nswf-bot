f45c81b4c3ba6344874d8ab5afa569b0aa64ae33
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-10 21:13:15 +0100

    Update asian.js

--- commands/asian.js
+++ commands/asian.js
@@ -4,7 +4,7 @@ const fs = require("fs")
 
 exports.run = (client, message, args) => {
     var subreddits = [
-        'asian',
+        'asianboobs',
         'asiangirl',
         'asianporn'
     ]
