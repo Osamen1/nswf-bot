7587eea1f7d0aa42d3bc3d4ed1a14f67d113bd71
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-10 19:19:06 +0100

    Update boobs.js

--- commands/boobs.js
+++ commands/boobs.js
@@ -1,18 +1,22 @@
-const Discord = require("discord.js");
+const randomPuppy = require('random-puppy');
 const request = require('snekfetch');
 const fs = require("fs")
 
 exports.run = (client, message, args) => {
-    var max = 12119;
-    var min = 10000;
-    var MathRan = Math.floor(Math.random() * (max - min + 0)) + min;
-    var MathLoL = Math.round(MathRan);
-        var randomname = Math.floor(Math.random() * (99999999999999999999 - 11111111111111111111 + 0)) + 11111111111111111111;
-        request.get("http://media.oboobs.ru/boobs_preview/" + MathLoL + ".jpg").then(r => {
-            fs.writeFile(`${randomname}.jpg`, r.body)
-            message.channel.sendFile(r.body).then(d => {
-                fs.unlink(`./${randomname}.jpg`)
+    var subreddits = [
+        'bigboobs',
+        'girlboobs',
+        'menboobs'
+    ]
+    var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+
+    randomPuppy(sub)
+            .then(url => {
+                var randomname = Math.floor(Math.random() * (99999999999999999999 - 11111111111111111111 + 0)) + 11111111111111111111;
+                request.get(url).then(r => {               
+                    fs.writeFile(`${randomname}.jpg`)
+                    message.channel.send(url).then(d => {
+                    })
             })
         })
-    }
 }
