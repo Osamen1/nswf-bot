c0d2a54d01131530be2a3399d2e12b7badef0da0
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-10 19:28:44 +0100

    Update gif.js

--- commands/gif.js
+++ commands/gif.js
@@ -19,9 +19,9 @@ exports.run = (client, message, args) => {
 
     randomPuppy(sub)
             .then(url => {
+                message.channel.send(url).then(d => {
                 const embed = new Discord.RichEmbed()
                     .setColor(0xffa500)
                     .setImage(url)
-                message.channel.send(url).then(d => {
         })
 }
