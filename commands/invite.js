784555bb21eed0cbc7c9b7f48869afd20381b17a
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-11 19:02:56 +0100

    Delete invite.js

--- commands/invite.js
+++
@@ -1,3 +0,0 @@
-exports.run = (client, message, args) => {
-    message.channel.send(`You can invite me with this link:\nhttps://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=51200&scope=bot`)
-}
