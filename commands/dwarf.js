2f9646d3f6d01a4452ac5d6a560f1aaca99a9619
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-11 18:43:40 +0100

    Update dwarf.js

--- commands/dwarf.js
+++ commands/dwarf.js
@@ -13,9 +13,10 @@ exports.run = (client, message, args) => {
     randomPuppy(sub)
             .then(url => {
                 var randomname = Math.floor(Math.random() * (99999999999999999999 - 11111111111111111111 + 0)) + 11111111111111111111;
-                request.get(url).then(r => {               
-                    fs.writeFile(`${randomname}.jpg`)
-                    message.channel.send(url).then(d => {
+                request.get(url).then(r => {
+                    fs.writeFile(`${randomname}.jpg`, r.body)
+                    message.channel.sendFile(r.body).then(d => {
+                        fs.unlink(`./${randomname}.jpg`)
                     })
             })
         })
