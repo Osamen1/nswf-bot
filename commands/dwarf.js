de9c66a00b3f7d51f5d88a8699aa8b1c9f80fc60
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-11 18:39:39 +0100

    Update dwarf.js

--- commands/dwarf.js
+++ commands/dwarf.js
@@ -4,9 +4,9 @@ const fs = require("fs")
 
 exports.run = (client, message, args) => {
     var subreddits = [
+        'dwarf',
         'dwarfs',
-        'porndwarfs',
-        'dwarf'
+        'dwarfsporn'
     ]
     var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
 
