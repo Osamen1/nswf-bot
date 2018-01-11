679c7f4c753aedf76b59009f094f094edd8bf1e2
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-11 18:57:26 +0100

    Update and rename nude.js to ble.js

--- commands/ble.js
+++ commands/ble.js
@@ -4,9 +4,9 @@ const fs = require("fs")
 
 exports.run = (client, message, args) => {
     var subreddits = [
-        'snapchatnudes',
-        'nudegirl',
-        'nudes'
+        'disgustinggirls',
+        'nudedisgustinggirls',
+        'girlsdisgusting'
     ]
     var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
 
