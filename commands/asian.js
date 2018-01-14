84f9c07043842f756c28928f971edbe9bfb12d5d
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-14 23:18:00 +0100

    Update asian.js

--- commands/asian.js
+++ commands/asian.js
@@ -5,7 +5,7 @@ const fs = require("fs")
 exports.run = (client, message, args) => {
     var subreddits = [
         'asianboobs',
-        'asiangirl',
+        'asian',
         'asianporn'
     ]
     var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
