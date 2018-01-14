0333d681e6604cdb7955f16c15737f682ba25264
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-14 23:23:56 +0100

    Update pornhub.js

--- commands/pornhub.js
+++ commands/pornhub.js
@@ -4,8 +4,8 @@ exports.run = (client, message, args) => {
     const query = args.join(' ')
     if (!query) return message.channel.send("Usage: nsfw.pornhub <search query>")
     const Pornsearch = require('pornsearch').default.search(query);
-        Pornsearch.gifs(1)
-            .then(gifs => {
+        Pornsearch.url(1)
+            .then(url => {
                 let gifrnd = gifs.map(gif => gif.url)
                 let embed = new Discord.RichEmbed()
                     .setImage(gifrnd[Math.floor(Math.random() * gifrnd.length)])
