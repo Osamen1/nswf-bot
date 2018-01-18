ea711c0aed1ab2240e055f21ce1471c12fc06c84
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-18 23:43:47 +0100

    Update losuj.js

--- commands/losuj.js
+++ commands/losuj.js
@@ -2,10 +2,28 @@ const request = require('snekfetch');
 const fs = require("fs")
 const Discord = require("discord.js")
 exports.run = (client, message, args) => {
+    var topics = [
+        'cum',
+        'cumshot',
+        'anal',
+        'oral',
+        'teen',
+        'tits',
+        "milf",
+        "creampie"
+    ]
+    const Pornsearch = require('pornsearch').default.search(topics[Math.round(Math.random() * (topics.length - 1))]);
+    
+    Pornsearch.gifs(1)
+    .then(gifs => {
+        let gifrnd = gifs.map(gif => gif.url)
+        if (message.mentions.users.first()) {
+            let embed = new Discord.RichEmbed()
+            .setTitle(`**${message.author.username}** fucked ${message.mentions.users.first().username}`)
             })
         } else {
             let embed = new Discord.RichEmbed()
-            .setTitle(`**${message.author.username}** dostał bana:( **')
+            .setTitle(`**${message.author.username}** wylosował bana :(**`)
             .setColor(0xFFA500)
             message.channel.send({
                 embed: embed
