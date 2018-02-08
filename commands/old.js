98c0fad263ae4cdc9644bb9269b0a09cf791c0f6
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-02-08 21:25:51 +0100

    Create old.js

---
+++ commands/old.js
@@ -0,0 +1,20 @@
+const randomPuppy = require('random-puppy');
+const request = require('snekfetch');
+const fs = require("fs")
+
+exports.run = (client, message, args) => {
+    var subreddits = [
+        'maturepornstar'
+    ]
+    var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+
+    randomPuppy(sub)
+            .then(url => {
+                var randomname = Math.floor(Math.random() * (99999999999999999999 - 11111111111111111111 + 0)) + 11111111111111111111;
+                request.get(url).then(r => {               
+                    fs.writeFile(`${randomname}.jpg`)
+                    message.channel.send(url).then(d => {
+                    })
+            })
+        })
+}
