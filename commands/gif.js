8e5ddb654ff2a606e4bc74b37cc4c5ff9405b26a
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-02-28 17:42:03 +0100

    Update gif.js

--- commands/gif.js
+++ commands/gif.js
@@ -24,4 +24,5 @@ exports.run = (client, message, args) => {
                     .setColor(0xffa500)
                     .setImage(url)
                     message.channel.send(url).then(d => {
-})
+        })
+}
