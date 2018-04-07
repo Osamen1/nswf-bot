7c9058d591870e3460575a3db8fc64529607ffc7
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-04-07 23:35:05 +0200

    Update gayporn.js

--- commands/gayporn.js
+++ commands/gayporn.js
@@ -5,7 +5,7 @@ const fs = require("fs")
 exports.run = (client, message, args) => {
       if (message.channel.nsfw === false) return message.reply(":warning: Ten kanał nie służy do tego :).");
     var subreddits = [
-        'Testingjunk'
+        'testingjunk'
     ]
     var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
 
