7968bbdd66d0cf733461d51e4e737028fcfb6d56
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-05-27 22:35:55 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -4,5 +4,5 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) => {
 
      if (fromID === me && text.includes("say") ) {
-    bot.sendMessage({ to: channel, message: txt.replace('say ','') });
+    bot.sendMessage({ to: channel, message: txt.replace('say ','') };
 }
