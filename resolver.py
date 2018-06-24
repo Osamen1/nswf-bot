9fcffb10d2840f3ab1b323d9c174011a2cb0993d
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-06-24 17:34:21 +0200

    Create resolver.py

---
+++ resolver.py
@@ -0,0 +1,20 @@
+# Enter Data
+tag=raw_input("Enter Discord Tag ")
+import time
+import progressbar
+
+bar = progressbar.ProgressBar()
+for i in bar(range(100)):
+    time.sleep(0.04)
+print'Found Discord Tag', tag
+usrid=raw_input("Enter User ID ")
+import time
+import progressbar
+
+bar = progressbar.ProgressBar()
+for i in bar(range(100)):
+    time.sleep(0.10)
+print'Found User ID', usrid
+
+
+
